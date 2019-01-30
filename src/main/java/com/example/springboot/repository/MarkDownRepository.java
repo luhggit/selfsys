e17a3e8a4e9e76e07fe59b099ed626da164ac276
luhg <342521162@qq.com>
2019-01-30 20:33:16 +0800

    add serach function

--- src/main/java/com/example/springboot/repository/MarkDownRepository.java
+++ src/main/java/com/example/springboot/repository/MarkDownRepository.java
@@ -2,9 +2,14 @@ package com.example.springboot.repository;
 
 import com.example.springboot.entity.MarkDown;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import java.util.List;
 
 public interface MarkDownRepository extends JpaRepository<MarkDown,Integer> {
     List<MarkDown> findMarkDownsByStatusIsNull();
+
+    List<MarkDown> findByMdContentLike(String like);
+
+    List<MarkDown> findMarkDownsByMdContentIsLike(String like);
 }
--- src/test/java/com/example/springboot/repository/MarkDownRepositoryTest.java
+++ src/test/java/com/example/springboot/repository/MarkDownRepositoryTest.java
@@ -1,7 +1,40 @@
 package com.example.springboot.repository;
 
+import com.example.springboot.SpringbootApplication;
+import com.example.springboot.entity.MarkDown;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import javax.annotation.Resource;
+
+import java.util.List;
+
 import static org.junit.Assert.*;
 
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringBootTest(classes = SpringbootApplication.class)
 public class MarkDownRepositoryTest {
 
+    @Resource
+    private MarkDownRepository markDownRepository;
+
+    /**
+     * 测试like
+     */
+    @Test
+    public void testLike() {
+        List<MarkDown> markDowns = markDownRepository.findByMdContentLike("%a%");
+        System.out.println(markDowns.size());
+    }
+
+    /**
+     * 测试islike
+     */
+    @Test
+    public void testIslike() {
+        List<MarkDown> markDowns = markDownRepository.findMarkDownsByMdContentIsLike("%a%");
+        System.out.println(markDowns.size());
+    }
 }
